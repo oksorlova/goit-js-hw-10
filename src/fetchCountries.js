ef8141ed1e598e1cb289725beeb58fd462350fbd
Oksana Orlova <kseniyaorlova88@gmail.com>
2022-02-01 16:30:02 +0300

    ДЗ 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,8 @@
-export default function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,languages`)
-    .then (data => {
+export default function fetchCountries(searchCountry) {
+  fetch(`https://restcountries.com/v3.1/name/${searchCountry}?fields=name,capital,population,flags,languages`)
+    .then ((data) => {
         return data.json();
-    })
+    }).catch ((error) => {
+        Notiflix.Notify.error('Oops, there is no country with that name');
+})
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import './css/styles.css';
-var debounce = require('lodash.debounce');
-import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce'
+import fetchCountries from './fetchCountries.js';
+import Notiflix from 'notiflix';
 
 
 const DEBOUNCE_DELAY = 300;
 
-const searchInput = document.querySelector('input#search-box');
+const searchInput = document.querySelector('#search-box');
+const counrtyList = document.querySelector ('.country-list');
+const countryInfo = document.querySelector ('.country-info');
 
-searchInput.addEventListener ('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+    let searchCountry = e.target.value.trim();
+    if (searchCountry === "") {
+        counrtyList.innerHTML ="";
+        countryInfo.innerHTML ="";
+        return;
+    }
+
+    fetchCountries(searchCountry).then((data) => {
+        if (data.length > 10) {
+            Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.');
+        }
+        else if (1 > data.length < 10) {
+            countriesMarkup(data);
+
+        }
+        if (data.length === 1) {
+            countryMarkup(data);
+
+        }
+
+    }
+)}
+        
+
+function countriesMarkup (countries) {
+    let countriesList = countries.map(elem => { return `<li>
+    <img class = "countries-info" src="${elem.flags.svg}" alt="${elem.name}">
+    ${elem.name}</li>` }).join("");
+
+    counrtyList.innerHTML= countriesList;   
+    
+    
+}
+
+function countryMarkup (countries) {
+    let countryItem = countries.map(elem => {
+    return `<img class = "countries-info" src="${elem.flags.svg}" alt="${elem.name}">
+    ${elem.name}>
+    <ul class = "countries-list">
+    <li class = "countries-item">Capital:${elem.capital}</li>
+    <li class = "countries-item">Population:${elem.population}</li>
+    <li class = "countries-item">Languages:${elem.languages}</li>
+    </ul>` }).join("");
+
+    countryInfo.innerHTML = countryItem;  
+    
+
+    
+}
 
 
